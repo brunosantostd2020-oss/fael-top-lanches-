e87ddf95866add1bce9a12c221bc465747f6d220
Bruno <SEU_EMAIL@gmail.com>
2026-04-20 08:37:36 -0300

    Correções finais: Modal + Privacidade + Troco

--- adicionar-frango-desfiado.sql
+++ adicionar-frango-desfiado.sql
@@ -3,27 +3,27 @@
 
 -- 1. X-FRANGO DESFIADO
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado', 'lanches', 18.00, 'Pão, frango desfiado, queijo, alface, tomate', true);
+VALUES ('🍗', 'X-Frango Desfiado', 'frango_desfiado', 18.00, 'Pão, frango desfiado, queijo, alface, tomate', true);
 
 -- 2. X-FRANGO DESFIADO ESPECIAL
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado Especial', 'lanches', 20.00, 'Pão, frango desfiado, queijo, bacon, alface, tomate, milho', true);
+VALUES ('🍗', 'X-Frango Desfiado Especial', 'frango_desfiado', 20.00, 'Pão, frango desfiado, queijo, bacon, alface, tomate, milho', true);
 
 -- 3. X-FRANGO DESFIADO CATUPIRY
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado Catupiry', 'lanches', 22.00, 'Pão, frango desfiado, catupiry, alface, tomate', true);
+VALUES ('🍗', 'X-Frango Desfiado Catupiry', 'frango_desfiado', 22.00, 'Pão, frango desfiado, catupiry, alface, tomate', true);
 
 -- 4. X-FRANGO DESFIADO BACON
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado Bacon', 'lanches', 21.00, 'Pão, frango desfiado, queijo, bacon, alface, tomate', true);
+VALUES ('🍗', 'X-Frango Desfiado Bacon', 'frango_desfiado', 21.00, 'Pão, frango desfiado, queijo, bacon, alface, tomate', true);
 
 -- 5. X-FRANGO DESFIADO COMPLETO
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado Completo', 'lanches', 25.00, 'Pão, frango desfiado, queijo, bacon, catupiry, milho, alface, tomate, azeitona', true);
+VALUES ('🍗', 'X-Frango Desfiado Completo', 'frango_desfiado', 25.00, 'Pão, frango desfiado, queijo, bacon, catupiry, milho, alface, tomate, azeitona', true);
 
 -- 6. X-FRANGO DESFIADO CHEDDAR
 INSERT INTO products (emoji, name, category, price, description, active)
-VALUES ('🍗', 'X-Frango Desfiado Cheddar', 'lanches', 23.00, 'Pão, frango desfiado, cheddar, bacon, alface, tomate', true);
+VALUES ('🍗', 'X-Frango Desfiado Cheddar', 'frango_desfiado', 23.00, 'Pão, frango desfiado, cheddar, bacon, alface, tomate', true);
 
 -- NOTA: Ajuste os preços conforme necessário!
 -- Para executar no PostgreSQL:
